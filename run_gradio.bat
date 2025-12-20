0e78b5014c18aa4292f57b8e8e57dcd76693c5c2
eXProfi <profiwork@gmail.com>
2025-12-21 01:36:45 +0200

    Update run_gradio.bat

--- run_gradio.bat
+++ run_gradio.bat
@@ -1,9 +1,9 @@
 @echo off
 :: Activate virtual environment
-call venv\Scripts\activate.bat
+cd .. && call venv\Scripts\activate.bat
 
 :: Launch app
-python gradio_app.py
+python fcm-parser\gradio_app.py
 
 :: Deactivate virtualenv after closing app
 venv\Scripts\deactivate.bat
