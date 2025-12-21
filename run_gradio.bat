2a62ed9515b09c0e23f7ee9dcd593401ec7236e4
eXProfi <114214417+eXProfi@users.noreply.github.com>
2025-12-21 04:24:57 +0200

    Delete run_gradio.bat

--- run_gradio.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-:: Activate virtual environment
-call venv\Scripts\activate.bat
-
-:: Launch app
-python gradio_app.py
-
-:: Deactivate virtualenv after closing app
-venv\Scripts\deactivate.bat
